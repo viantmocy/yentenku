7f8e9a74807925594319bfc4d9ed791145c293e7
viantmocy <vian.tmocy@gmail.com>
2025-09-19 18:54:37 +0700

    Initial commit: Yenten miner Termux setup

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# === Yenten Miner Start Script ===
+# sayangku leafia 💕
+
+# Jalankan miner dengan pool & wallet
+./sugarmaker_arm64 \
+  -a yespowerr16 \
+  -o stratum+tcp://62.72.45.90:16195 \
+  -u Ymv1PyMUVHL3ybXX9ABNuNTcq2HudTe4XW \
+  -p x
